d7c2429fe9690bb3f99689e57a0a7f643adb26a1
ASUS <ASUS@LAPTOP-U22KE66F>
2024-05-24 19:12:34 +0700

    konek

--- perpustakaan/src/koneksi/connector.java
+++ perpustakaan/src/koneksi/connector.java
@@ -3,11 +3,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package koneksi;
-
+import com.mysql.cj.jdbc.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 /**
  *
  * @author HP
  */
 public class connector {
-    
+    static Connection con;
+    public static Connection connection(){
+        if(con==null){
+            MysqlDataSource data = new MysqlDataSource();
+            data.setDatabaseName("movie_db");
+            data.setUser("root");
+            data.setPassword("");
+            try{
+                con=data.getConnection();
+                System.out.println("Koneksi berhasil");
+            }catch(SQLException ex){
+                ex.printStackTrace();
+                System.out.println("Koneksi gagal");
+            }
+        }
+        return con;
+    }
 }

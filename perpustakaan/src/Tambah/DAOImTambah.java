f60fa9a83db940dc2fb9c61c5e3132e74f7a4aa0
mangopancake <arrafinuristiawan445@gmail.com>
2024-05-27 13:11:28 +0700

    coba ini

---
+++ perpustakaan/src/Tambah/DAOImTambah.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Tambah;
+import java.util.List;
+import perpustakaan.*;
+/**
+ *
+ * @author ASUS
+ */
+public interface DAOImTambah {
+    public void create(dataBuku p);
+    public void edit(dataBuku p);
+    public void delete(dataBuku p);
+    List<dataBuku> getALL();
+}
+
+//buat coba commit hapus aja ntar, harus diedit soalnya
